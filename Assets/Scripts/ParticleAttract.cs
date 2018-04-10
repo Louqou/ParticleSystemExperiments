21effaca396e9082e7d71f46d0aeb3e9a29e3c9c
Drinity <mattbutcher93@hotmail.com>
2018-04-10 18:43:41 +0100

    First commit

---
+++ Assets/Scripts/ParticleAttract.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+[ExecuteInEditMode]
+public class ParticleAttract : MonoBehaviour
+{
+    public Transform target;
+    public float attractForce;
+
+    private new ParticleSystem particleSystem;
+    private Vector3 targetPos;
+    private ParticleSystem.Particle[] particles;
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+
+        if (particleSystem.main.simulationSpace == ParticleSystemSimulationSpace.Local
+            || particleSystem.main.simulationSpace == ParticleSystemSimulationSpace.Custom) {
+            targetPos = particleSystem.transform.InverseTransformPoint(target.position);
+        }
+        else {
+            targetPos = target.transform.position;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (particles == null || particles.Length < particleSystem.main.maxParticles) {
+            particles = new ParticleSystem.Particle[particleSystem.main.maxParticles];
+        }
+
+        particleSystem.GetParticles(particles);
+
+        float impulse = attractForce * Time.deltaTime;
+        for (int i = 0; i < particles.Length; i++) {
+            Vector3 direction = Vector3.Normalize(targetPos - particles[i].position);
+            Vector3 changeInDirection = direction * impulse;
+            particles[i].velocity += changeInDirection;
+        }
+
+        particleSystem.SetParticles(particles, particles.Length);
+    }
+}
---
+++ Assets/Scripts/ParticleFloor.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleFloor : MonoBehaviour
+{
+    public float pushSpeed = 10f;
+    public int xLength = 50;
+    public int yLength = 50;
+    private new ParticleSystem particleSystem;
+    private List<ParticleSystem.Particle> triggerParticles;
+
+    private void Start()
+    {
+        triggerParticles = new List<ParticleSystem.Particle>();
+        particleSystem = GetComponent<ParticleSystem>();
+
+        int numParticles = xLength * yLength;
+
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[numParticles];
+        ParticleSystem.MainModule particleSystemMain = particleSystem.main;
+        particleSystemMain.maxParticles = numParticles;
+        particleSystem.Emit(numParticles);
+        particleSystem.GetParticles(particles);
+
+        // Lets assume particle x y dimension is 1x1
+        for (int i = 0; i < xLength; i++) {
+            for (int j = 0; j < yLength; j++) {
+                particles[(i * yLength) + j].position = new Vector3(i, j);
+                particles[(i * yLength) + j].randomValue = 0f;
+            }
+        }
+
+        particleSystem.SetParticles(particles, numParticles);
+    }
+
+    private void OnParticleTrigger()
+    {
+        ParticlesInsideTrigger();
+        ParticlesOutsideTrigger();
+    }
+
+    private void ParticlesInsideTrigger()
+    {
+        int numParticles = particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, triggerParticles);
+        float pushAmount = -pushSpeed * Time.deltaTime;
+        for (int i = 0; i < numParticles; i++) {
+            ParticleSystem.Particle particle = triggerParticles[i];
+            particle.position = new Vector3(particle.position.x, particle.position.y, particle.position.z + pushAmount);
+            particle.randomValue += pushAmount;
+            triggerParticles[i] = particle;
+        }
+
+        particleSystem.SetTriggerParticles(ParticleSystemTriggerEventType.Inside, triggerParticles);
+    }
+
+    private void ParticlesOutsideTrigger()
+    {
+        int numParticles = particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Outside, triggerParticles);
+        float pushAmount = pushSpeed * Time.deltaTime;
+        for (int i = 0; i < numParticles; i++) {
+            ParticleSystem.Particle particle = triggerParticles[i];
+            float movedAmount = particle.randomValue;
+            if (movedAmount != 0) {
+                float dist = Mathf.Min(pushAmount, -movedAmount);
+                particle.position = new Vector3(
+                    triggerParticles[i].position.x,
+                    triggerParticles[i].position.y,
+                    triggerParticles[i].position.z + dist);
+                particle.randomValue += dist;
+                triggerParticles[i] = particle;
+            }
+        }
+
+        particleSystem.SetTriggerParticles(ParticleSystemTriggerEventType.Outside, triggerParticles);
+    }
+}
